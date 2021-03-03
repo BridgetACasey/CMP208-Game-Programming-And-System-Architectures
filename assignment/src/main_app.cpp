552aaf82ee7155b910c4415decdc61dc6f1b2797
BridgetACasey <bridgetannabelcasey@yahoo.com>
2021-03-03 19:22:30 +0000

    initial commit

---
+++ assignment/src/main_app.cpp
@@ -0,0 +1,101 @@
+#include "main_app.h"
+#include <system/platform.h>
+#include <graphics/sprite_renderer.h>
+#include <graphics/font.h>
+#include <system/debug_log.h>
+#include <graphics/renderer_3d.h>
+#include <maths/math_utils.h>
+
+
+MainApp::MainApp(gef::Platform& platform) :
+	Application(platform),
+	sprite_renderer_(NULL),
+	renderer_3d_(NULL),
+	primitive_builder_(NULL),
+	font_(NULL)
+{
+}
+
+void MainApp::Init()
+{
+	sprite_renderer_ = gef::SpriteRenderer::Create(platform_);
+
+	// create the renderer for draw 3D geometry
+	renderer_3d_ = gef::Renderer3D::Create(platform_);
+
+	// initialise primitive builder to make create some 3D geometry easier
+	primitive_builder_ = new PrimitiveBuilder(platform_);
+
+	InitFont();
+}
+
+void MainApp::CleanUp()
+{
+	CleanUpFont();
+
+	delete primitive_builder_;
+	primitive_builder_ = NULL;
+
+	delete renderer_3d_;
+	renderer_3d_ = NULL;
+
+	delete sprite_renderer_;
+	sprite_renderer_ = NULL;
+}
+
+bool MainApp::Update(float frame_time)
+{
+	fps_ = 1.0f / frame_time;
+
+
+	return true;
+}
+
+void MainApp::Render()
+{
+	// draw 3d geometry
+	renderer_3d_->Begin();
+
+	renderer_3d_->End();
+
+	// start drawing sprites, but don't clear the frame buffer
+	sprite_renderer_->Begin(false);
+	DrawHUD();
+	sprite_renderer_->End();
+}
+void MainApp::InitFont()
+{
+	font_ = new gef::Font(platform_);
+	font_->Load("comic_sans");
+}
+
+void MainApp::CleanUpFont()
+{
+	delete font_;
+	font_ = NULL;
+}
+
+void MainApp::DrawHUD()
+{
+	if(font_)
+	{
+		// display frame rate
+		font_->RenderText(sprite_renderer_, gef::Vector4(850.0f, 510.0f, -0.9f), 1.0f, 0xffffffff, gef::TJ_LEFT, "FPS: %.1f", fps_);
+	}
+}
+
+void MainApp::SetupLights()
+{
+	// grab the data for the default shader used for rendering 3D geometry
+	gef::Default3DShaderData& default_shader_data = renderer_3d_->default_shader_data();
+
+	// set the ambient light
+	default_shader_data.set_ambient_light_colour(gef::Colour(0.25f, 0.25f, 0.25f, 1.0f));
+
+	// add a point light that is almost white, but with a blue tinge
+	// the position of the light is set far away so it acts light a directional light
+	gef::PointLight default_point_light;
+	default_point_light.set_colour(gef::Colour(0.7f, 0.7f, 1.0f, 1.0f));
+	default_point_light.set_position(gef::Vector4(-500.0f, 400.0f, 700.0f));
+	default_shader_data.AddPointLight(default_point_light);
+}
---
+++ assignment/src/main_app.h
@@ -0,0 +1,42 @@
+#ifndef _SCENE_APP_H
+#define _SCENE_APP_H
+
+#include <system/application.h>
+#include <maths/vector2.h>
+#include "primitive_builder.h"
+#include <graphics/mesh_instance.h>
+
+// FRAMEWORK FORWARD DECLARATIONS
+namespace gef
+{
+	class Platform;
+	class SpriteRenderer;
+	class Font;
+	class InputManager;
+	class Renderer3D;
+}
+
+class MainApp : public gef::Application
+{
+public:
+	MainApp(gef::Platform& platform);
+	void Init();
+	void CleanUp();
+	bool Update(float frame_time);
+	void Render();
+private:
+	void InitFont();
+	void CleanUpFont();
+	void DrawHUD();
+	void SetupLights();
+
+	gef::SpriteRenderer* sprite_renderer_;
+	gef::Font* font_;
+	gef::Renderer3D* renderer_3d_;
+
+	PrimitiveBuilder* primitive_builder_;
+
+	float fps_;
+};
+
+#endif // _SCENE_APP_H
