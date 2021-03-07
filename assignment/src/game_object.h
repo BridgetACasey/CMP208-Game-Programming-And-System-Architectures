44fd6f190077c3883a005464d63b0e34d0941d32
BridgetACasey <bridgetannabelcasey@yahoo.com>
2021-03-07 14:08:06 +0000

    added assets folder

---
+++ assignment/src/game_object.h
@@ -0,0 +1,39 @@
+#ifndef _GAME_OBJECT_H
+#define _GAME_OBJECT_H
+
+#include <graphics/mesh_instance.h>
+#include <box2d/Box2D.h>
+
+namespace gef
+{
+	
+}
+
+class PrimitiveBuilder;
+
+class GameObject : public gef::MeshInstance
+{
+protected:
+	GameObject();
+
+public:
+	~GameObject();
+
+	static GameObject* Create();
+
+	void SetMeshInstance(PrimitiveBuilder* primitive_builder);
+
+	void SetPosition(float x, float y, float z);
+	void SetRotation(float x, float y, float z);
+	void SetScale(float x, float y, float z);
+
+	void BuildTransform();
+
+private:
+	gef::Vector4 position;
+	gef::Vector4 rotation;
+	gef::Vector4 scale;
+
+};
+
+#endif // _GAME_OBJECT_H
